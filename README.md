1cf546874f9cd83b6a99676118b82c3b3608116b
Josh Kropf <josh@slashdev.ca>
2024-11-08 10:56:44 -0500

    Use dt overlay and gpio descriptor api to limit hard coding

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Linux kernel module to read buttons states from pi2jamma and report key presses.
+
+## Usage
+
+```bash
+make
+dtoverlay pi2jamma_input_overlay.dtbo
+insmod pi2jamma_input.ko
+```
--- src/pi2jamma_input.c
+++ src/pi2jamma_input.c
@@ -1,23 +1,18 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/input.h>
-#include <linux/gpio.h>
-// TODO figure out how to use new API, <linux/gpio.h> is depricated
-// #include <linux/gpio/consumer.h>
+#include <linux/property.h>
+#include <linux/platform_device.h>
+#include <linux/gpio/consumer.h>
 #include <linux/delay.h>
 
-// #define _74x165_CLK 23 // Pin 16
-// #define _74x165_PL  24 // Pin 18
-// #define _74x165_DIN 22 // Pin 15
-// use `cat /sys/kernel/debug/gpio` to find gpio number, e.g. gpio-535 (GPIO23)
-#define _74x165_CLK 535 // Pin 16
-#define _74x165_PL  536 // Pin 18
-#define _74x165_DIN 534 // Pin 15
+// 3x shirft registers, 8 bits each
 #define _74x165_BITS 24
 
-static struct input_dev *pi2jamma_dev;
+static struct input_dev *input_dev;
+static struct gpio_desc *clk_gpio, *pl_gpio, *din_gpio;
 
-/* keymap is in order of bits read from pi2jamma */
+// keymap is in order of bits read from pi2jamma
 static int keymap[_74x165_BITS] = {
     KEY_SPACE,     // P1B3
     KEY_LEFTSHIFT, // P1B4
@@ -48,24 +43,24 @@ static int keymap[_74x165_BITS] = {
 static uint32_t pi2jamma_read_buttons(void) {
     uint32_t result = 0;
 
-    gpio_set_value(_74x165_CLK, 1);
+    gpiod_set_value(clk_gpio, 1);
     udelay(2);
-    gpio_set_value(_74x165_PL, 0);
+    gpiod_set_value(pl_gpio, 0);
     udelay(2);
-    gpio_set_value(_74x165_PL, 1);
+    gpiod_set_value(pl_gpio, 1);
 
     for (int i=0; i<_74x165_BITS; i++) {
         result = result << 1;
 
         udelay(2);
-        uint8_t value = gpio_get_value(_74x165_DIN);
+        uint8_t value = gpiod_get_value(din_gpio);
 
         // key state: 1 = off, 0 = on
         result = result | (!value);
 
-        gpio_set_value(_74x165_CLK, 0);
+        gpiod_set_value(clk_gpio, 0);
         udelay(2);
-        gpio_set_value(_74x165_CLK, 1);
+        gpiod_set_value(clk_gpio, 1);
     }
 
     return result;
@@ -76,115 +71,120 @@ static void pi2jamma_poller(struct input_dev *input) {
 
     bits = pi2jamma_read_buttons();
 
-    // if (bits) {
-    //     printk("Non zero bits %x\n", bits);
-    // }
-
     for (int i=0; i<_74x165_BITS; i++) {
-        input_report_key(pi2jamma_dev, keymap[i], bits & 1);
+        input_report_key(input_dev, keymap[i], bits & 1);
         bits = bits >> 1;
     }
 
-    input_sync(pi2jamma_dev);
-
+    input_sync(input_dev);
 }
 
-static int __init pi2jamma_init(void) {
-    int error;
-    int gpio_init[3] = { 0, 0, 0 };
+static const struct of_device_id pi2jamma_input_of_match[] = {
+    { .compatible = "pi2jamma-input", },
+    { },
+};
+MODULE_DEVICE_TABLE(of, pi2jamma_input_of_match);
 
-    printk("pi2jamma_input init\n");
+static int pi2jamma_probe(struct platform_device *pdev) {
+    struct device *dev = &pdev->dev;
+    int poll_interval, error;
 
-    pi2jamma_dev = input_allocate_device();
-    if (!pi2jamma_dev) {
+    if (device_property_read_u32(dev, "poll-interval", &poll_interval)) {
+        printk(KERN_ERR "pi2jamma_input: Failed to read 'poll-interval' property\n");
+        return -ENOMEM;
+    }
+
+    clk_gpio = gpiod_get(dev, "clk", GPIOD_OUT_HIGH);
+    pl_gpio = gpiod_get(dev, "pl", GPIOD_OUT_HIGH);
+    din_gpio = gpiod_get(dev, "din", GPIOD_IN);
+
+    if (IS_ERR(clk_gpio)) {
+        error = PTR_ERR(clk_gpio);
+        printk(KERN_ERR "pi2jamma_input: Failed to setup clk gpio\n");
+        goto err_free_gpio;
+    }
+
+    if (IS_ERR(pl_gpio)) {
+        error = PTR_ERR(pl_gpio);
+        printk(KERN_ERR "pi2jamma_input: Failed to setup pl gpio\n");
+        goto err_free_gpio;
+    }
+
+    if (IS_ERR(din_gpio)) {
+        error = PTR_ERR(din_gpio);
+        printk(KERN_ERR "pi2jamma_input: Failed to setup din gpio\n");
+        goto err_free_gpio;
+    }
+
+    // devm_input_allocate_device() creates a managed instance that does not
+    // need to be explicitly free'd or unregistered
+    // using input_allocate_device() here causes input_register_device() to block
+    input_dev = devm_input_allocate_device(dev);
+    if (!input_dev) {
         printk(KERN_ERR "pi2jamma_input: Not enough memory\n");
         error = -ENOMEM;
-        goto err_return;
+        goto err_free_gpio;
     }
 
-    pi2jamma_dev->evbit[0] = BIT_MASK(EV_KEY);
-    // set_bit(EV_REP, pi2jamma_dev->evbit);
+    // input_dev->evbit[0] = BIT_MASK(EV_KEY);
+    set_bit(EV_KEY, input_dev->evbit);
+    // set_bit(EV_REP, input_dev->evbit);
     for (int i=0; i<_74x165_BITS; i++) {
-        set_bit(keymap[i], pi2jamma_dev->keybit);
+        set_bit(keymap[i], input_dev->keybit);
     }
 
-    error = input_setup_polling(pi2jamma_dev, pi2jamma_poller);
+    // using input subsystem polling is much nicer on CPU usage vs creating a
+    // thread and doing our own polling
+    error = input_setup_polling(input_dev, pi2jamma_poller);
     if (error) {
-        printk(KERN_ERR "pi2jamma_input: Unable to set up polling\n");
-        goto err_return;
+        printk(KERN_ERR "pi2jamma_input: Failed to setup input polling\n");
+        goto err_free_gpio;
     }
 
     // poll interval in millis
-    input_set_poll_interval(pi2jamma_dev, 10);
+    input_set_poll_interval(input_dev, poll_interval);
 
-    error = input_register_device(pi2jamma_dev);
+    error = input_register_device(input_dev);
     if (error) {
-        printk(KERN_ERR "pi2jamma_input: Failed to register device\n");
-        goto err_free_dev;
-    }
-
-    // bcm2835_gpio_fsel(CLK, BCM2835_GPIO_FSEL_OUTP);
-    // bcm2835_gpio_fsel(PL, BCM2835_GPIO_FSEL_OUTP);
-    // bcm2835_gpio_fsel(DIN, BCM2835_GPIO_FSEL_INPT);
-
-    error = gpio_request(_74x165_CLK, "74x165-clk");
-    if (error) {
-        printk(KERN_ERR "pi2jamma_input: Failed to request gpio for CLK\n");
-        goto err_free_gpio;
-    }
-    gpio_init[0] = _74x165_CLK;
-    error = gpio_direction_output(_74x165_CLK, 0);
-    if (error) {
-        printk(KERN_ERR "pi2jamma_input: Failed to set gpio direction for CLK\n");
-        goto err_free_gpio;
-    }
-
-    error = gpio_request(_74x165_PL, "74x165-pl");
-    if (error) {
-        printk(KERN_ERR "pi2jamma_input: Failed to request gpio for PL\n");
-        goto err_free_gpio;
-    }
-    gpio_init[1] = _74x165_PL;
-    error = gpio_direction_output(_74x165_PL, 0);
-    if (error) {
-        printk(KERN_ERR "pi2jamma_input: Failed to set gpio direction for PL\n");
-        goto err_free_gpio;
-    }
-
-    error = gpio_request(_74x165_DIN, "74x165-din");
-    if (error) {
-        printk(KERN_ERR "pi2jamma_input: Failed to request gpio for DIN\n");
-        goto err_free_gpio;
-    }
-    gpio_init[2] = _74x165_DIN;
-    error = gpio_direction_input(_74x165_DIN);
-    if (error) {
-        printk(KERN_ERR "pi2jamma_input: Failed to set gpio direction for DIN\n");
+        printk(KERN_ERR "pi2jamma_input: Failed to register input device\n");
         goto err_free_gpio;
     }
 
     return 0;
 
 err_free_gpio:
-    for (int i = 0; i<3; i++) {
-        if (gpio_init[i]) {
-            gpio_free(gpio_init[i]);
-        }
-    }
-err_free_dev:
-    input_free_device(pi2jamma_dev);
-err_return:
+    if (!IS_ERR(clk_gpio)) gpiod_put(clk_gpio);
+    if (!IS_ERR(pl_gpio)) gpiod_put(pl_gpio);
+    if (!IS_ERR(din_gpio)) gpiod_put(din_gpio);
+
     return error;
 }
 
+static void pi2jamma_shutdown(struct platform_device *pdev) {
+    gpiod_put(clk_gpio);
+    gpiod_put(pl_gpio);
+    gpiod_put(din_gpio);
+}
+
+static struct platform_driver pi2jamma_input_device_driver = {
+    .probe = pi2jamma_probe,
+    .remove = pi2jamma_shutdown,
+    .driver = {
+        .name = "pi2jamma",
+        .of_match_table = pi2jamma_input_of_match
+    }
+};
+
+static int __init pi2jamma_init(void) {
+    return platform_driver_register(&pi2jamma_input_device_driver);
+}
+
 static void __exit pi2jamma_exit(void) {
-    gpio_free(_74x165_CLK);
-    gpio_free(_74x165_PL);
-    gpio_free(_74x165_DIN);
-    input_unregister_device(pi2jamma_dev);
+    platform_driver_unregister(&pi2jamma_input_device_driver);
 }
 
 module_init(pi2jamma_init);
 module_exit(pi2jamma_exit);
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Josh Kropf");
